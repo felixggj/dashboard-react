e80bc289097f03ecebd6fc84328becc16309703d
felixggj <me@felix-gomez.com>
2023-06-25 13:33:48 +0200

    Structured file system between components and established base styling

---
+++ src/themes.js
@@ -0,0 +1,13 @@
+// Setup to get all the colours and the types/typographies of the website, and we are going to allow this to be light and dark mode
+
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+// colour design tokens
+
+#666666
+#141b2d
+#4cceac
+#db4f4a
+#6870fa
+
